8a67b316767008bd912d6d383de8a4115dd9317d
SvIlina <svitlanailina@gmail.com>
2019-06-12 20:48:14 +0300

    Initial commit

---
+++ src/main/java/loginInformation/UserLogInPage.java
@@ -0,0 +1,42 @@
+package loginInformation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class UserLogInPage {
+
+    @FindBy(id = "email")
+    private WebElement emailAddress;
+
+    @FindBy(id = "passwd")
+    private WebElement password;
+
+    @FindBy(id = "SubmitLogin")
+    private WebElement signIn;
+
+    @FindBy(xpath = "//li[text() ='Invalid email address.']")
+    private WebElement errorMessage;
+
+    public UserLogInPage(WebDriver webDriver) {
+        PageFactory.initElements(webDriver, this);
+    }
+
+    public void inputUserEmail(String email) {
+        emailAddress.sendKeys(email);
+    }
+
+    public void inputUserPassword(String passwd) {
+        password.sendKeys(passwd);
+    }
+
+    public void submitLogIn() {
+        signIn.submit();
+    }
+
+    public String getErrorMessage() {
+        return errorMessage.getText();
+    }
+}
+
---
+++ src/test/java/signInTest/SignInTest.java
@@ -0,0 +1,51 @@
+package signInTest;
+
+import loginInformation.UserLogInPage;
+import loginInformation.UserLogInPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class SignInTest {
+
+    private WebDriver webDriver;
+
+    @BeforeTest
+    public void BeforeTestSignIn() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        webDriver.manage().window().maximize();
+        webDriver.get("http://automationpractice.com/index.php");
+        WebElement clickSignIn = webDriver.findElement(By.xpath("//*[contains(@title,'Log in to your customer account')]"));
+        clickSignIn.click();
+    }
+
+    @Test
+    public void UserSignInTest() {
+        UserLogInPage userLogIn = new UserLogInPage(webDriver);
+        userLogIn.inputUserEmail("iteaiteatester@gmail.com");
+        userLogIn.inputUserPassword("test1234");
+        userLogIn.submitLogIn();
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void UserSignInValidation() {
+        webDriver.get("http://automationpractice.com/index.php");
+        UserLogInPage userLogInPage = new UserLogInPage(webDriver);
+        userLogInPage.inputUserEmail("invalid_email");
+        userLogInPage.inputUserPassword("fszjcf");
+        userLogInPage.submitLogIn();
+        String expectedResult = "Invalid email address.";
+        String actualResult = userLogInPage.getErrorMessage();
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+}
+
